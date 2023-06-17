ac167d0db4bc85c70e1d547cceb5e60ab53fb88a
RomanaDiana <peshokhardr@gmail.com>
2023-06-17 22:34:15 +0300

    ???

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -147,7 +147,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
